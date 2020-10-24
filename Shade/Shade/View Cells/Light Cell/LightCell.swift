bcae24ab4335312cf207645f42753a3d7502aa96
CharlieWhile13 <mrcharliewhile@gmail.com>
2020-10-24 16:13:02 +0100

    generate your files lazy cunt

---
+++ Shade/Shade/View Cells/Light Cell/LightCell.swift
@@ -0,0 +1,17 @@
+//
+//  LightCell.swift
+//  Shade
+//
+//  Created by Charlie While on 24/10/2020.
+//
+
+import UIKit
+
+class LightCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
