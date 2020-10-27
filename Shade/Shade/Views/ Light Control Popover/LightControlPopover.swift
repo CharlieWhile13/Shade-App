ed731d8de3336e925c213881b50cc770e6196347
CharlieWhile13 <mrcharliewhile@gmail.com>
2020-10-27 00:54:18 +0000

    uwu

---
+++ Shade/Shade/Views/ Light Control Popover/LightControlPopover.swift
@@ -0,0 +1,20 @@
+//
+//  LightControlPopover.swift
+//  Shade
+//
+//  Created by Charlie While on 26/10/2020.
+//
+
+import UIKit
+
+class LightControlPopover: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
